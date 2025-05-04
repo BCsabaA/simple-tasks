7fa2ed2b5c03ee16d8beb80dfbb9b8e1c5d08774
BCsabaA <barabas.csabi@gmail.com>
2025-05-04 13:26:12 +0200

    add task screen implementation started

--- simple_tasks/textuals/app_screens.py
+++ simple_tasks/textuals/app_screens.py
@@ -1,7 +1,7 @@
 from textual.app import App, ComposeResult
 from textual.screen import Screen
 from textual.widgets import Header, Footer
-from textuals.custom_screens import QuitScreen
+from textuals.custom_screens import QuitScreen, FormScreen
 
 from set_logger import set_logger
 
@@ -11,6 +11,7 @@ logger = set_logger(__name__)
 class MainScreen(Screen):
     BINDINGS = [
         ("q", "request_quit", "Quit"),
+        ("a", "add_task", "Add"),
     ]
 
     def compose(self) -> ComposeResult:
@@ -21,3 +22,22 @@ class MainScreen(Screen):
 
     def action_request_quit(self):
         self.app.push_screen(QuitScreen())
+
+    def action_add_task(self):
+        def check_inputs(inputs: dict[str]) -> None:
+            pass
+
+        self.app.push_screen(
+            FormScreen(
+                (
+                    {'name': ''},
+                    {'description': ''},
+                    {'start date': ''},
+                    {'deadline': ''},
+                    {'priority': ''},
+                )
+            ),
+            check_inputs
+        )
+
+
